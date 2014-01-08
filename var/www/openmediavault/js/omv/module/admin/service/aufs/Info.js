604793a3312471cf14e5cdc601d6bde60b09fc1f
Aaron Murray <aaron@omv-extras.org>
2014-01-08 08:39:44 -0600

    add options

--- var/www/openmediavault/js/omv/module/admin/service/aufs/Info.js
+++ var/www/openmediavault/js/omv/module/admin/service/aufs/Info.js
@@ -25,50 +25,35 @@ Ext.define("OMV.module.admin.service.aufs.Info", {
     initComponent: function() {
         var me = this;
 
-        me.html = "
-<b>User's Direct Branch Access (UDBA)</b>
-<p>
-    UDBA means a modification to a branch filesystem manually or directly, e.g. bypassing aufs. While aufs is designed and implemented 
-    to be safe after UDBA, it can make yourself and your aufs confused. And some information like aufs inode will be incorrect. For 
-    example, if you rename a file on a branch directly, the file on aufs may or may not be accessible through both of old and new name. 
-    Because aufs caches various information about the files on branches. And the cache still remains after UDBA. Aufs has a mount option 
-    named 'udba' which specifies the test level at access time whether UDBA was happened or not.
-</p>
-<ul>
-    <li>
-        udba=reval
-        <br/><br/>
-        Aufs tests only the existence of the file which existed. If the existed file was removed on the branch directly, aufs discard 
-        the cache about the file and re-lookup it. So the data will be updated. This test is at minimum level to keep the performance 
-        and ensure the existence of a file. This is default and aufs runs still fast.
-    </li>
-    <li>
-        udba=notify
-        <br/><br/>
-        Aufs sets either 'fsnotify' or 'inotify' to all the accessed directories on its branches and receives the event about the dir 
-        and its children. It consumes resources, cpu and memory. Performance will be hurt, but it is most strict test level. When a 
-        user accesses the file which was notified UDBA before, the cached data about the file will be discarded and aufs will re-lookup
-        it. 
-
-    </li>
-</ul>
-
-<b>Most-Free-Space (MFS)</b>
-<ul>
-    <li>
-        create=mfs
-        <br/><br/>
-        Selects a writable branch which has most free space.
-    </li>
-    <li>
-        create=pmfs
-        <br/><br/>
-        Selects a writable branch where the parent dir exists. When the parent dir exists on multiple writable branches, aufs selects the one 
-        which has more free space.
-    </li>
-</ul>
-        ";
-
+        me.html = "<b>" + _("User's Direct Branch Access (UDBA)") + "</b>" +
+                  "<p>" +
+                    _("UDBA means a modification to a branch filesystem manually or directly, e.g. bypassing aufs. While aufs is designed and implemented to be safe after UDBA, it can make yourself and your aufs confused. And some information like aufs inode will be incorrect. For example, if you rename a file on a branch directly, the file on aufs may or may not be accessible through both of old and new name. Because aufs caches various information about the files on branches. And the cache still remains after UDBA. Aufs has a mount option named 'udba' which specifies the test level at access time whether UDBA was happened or not.") +
+                  "</p>" +
+                  "<ul>" +
+                    "<li>" +
+                      _("udba=reval") +
+                      "<br/><br/>" +
+                      _("Aufs tests only the existence of the file which existed. If the existed file was removed on the branch directly, aufs discard the cache about the file and re-lookup it. So the data will be updated. This test is at minimum level to keep the performance and ensure the existence of a file. This is default and aufs runs still fast.") +
+                    "</li>" +
+                    "<li>" +
+                      _("udba=notify") +
+                      "<br/><br/>" +
+                      _("Aufs sets either 'fsnotify' or 'inotify' to all the accessed directories on its branches and receives the event about the dir and its children. It consumes resources, cpu and memory. Performance will be hurt, but it is most strict test level. When a user accesses the file which was notified UDBA before, the cached data about the file will be discarded and aufs will re-lookup it.") +
+                    "</li>" +
+                  "</ul>" +
+                  "<b>" + _("Most-Free-Space (MFS)") + "</b>" +
+                  "<ul>" +
+                    "<li>" +
+                      _("create=mfs") +
+                      "<br/><br/>" +
+                      _("Selects a writable branch which has most free space.") +
+                    "</li>" +
+                    "<li>" +
+                      _("create=pmfs") +
+                      "<br/><br/>" +
+                      _("Selects a writable branch where the parent dir exists. When the parent dir exists on multiple writable branches, aufs selects the one which has more free space.") +
+                    "</li>" +
+                  "</ul>";
         me.callParent(arguments);
     }
 });
